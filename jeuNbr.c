784296ec67183011eef96f1e70943ad45d28f66f
Ludovic DARCISSAC <askme@secret.email>
2018-09-10 16:29:17 +0200

    Commit de Ludo

--- jeuNbr.c
+++ jeuNbr.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main()
 {
     int i;
     int nbrChoix;
     srand(time(NULL));
-    int nbrAleatoire = rand();
-    nbrAleatoire = nbrAleatoire%10+1;
+    int nbrAleatoire = rand(), nbMax = 1000, nbEssais = 10 ;
+    nbrAleatoire = nbrAleatoire % nbMax + 1;
     printf("************* Bienvenue dans le jeu *************\n");
     printf("-------------------------------------------------\n");
-    printf("Trouvez le nombre secret est compris entre 1 et 10\n");
-    printf("Vous avez 3 essais\n");
+    printf("Trouvez le nombre secret est compris entre 1 et %d\n", nbMax);
+    printf("Vous avez %d essais\n", nbEssais);
     //printf("%d",nbrAleatoire);
 
-    for(i=2;i>=0;i--)
+    char dejaTestes[1000] = "Nombres déjà testés :" ;
+
+    for(i=nbEssais-1;i>=0;--i)
     {
-        printf("Entrez un nbr entre 1 et 10 : ");
+        printf("Entrez un nbr entre 1 et %d : ", nbMax);
         scanf("%d", &nbrChoix);
         if(nbrChoix==nbrAleatoire)
         {
@@ -33,6 +36,14 @@ int main()
             printf("\nNbr trop grand !\n");
         }
         printf("Il vous reste %d essais... \n", i);
+
+		  char strNbrChoix[12] ;
+		  sprintf(strNbrChoix, "%d", nbrChoix) ;
+		  strcat(dejaTestes, " ") ;
+		  strcat(dejaTestes, strNbrChoix) ;
+		  
+		  printf("%s\n", dejaTestes) ;
+
     }
     printf("\n--- :( PERDU :( ---\n");
     return 0;
